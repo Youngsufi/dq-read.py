6e0d825ab395e7a05fea61dcc4f838520a742f9f
hameed <8092249+Youngsufi@users.noreply.github.com>
2019-01-16 23:16:10 -0500

    Create read.py

---
+++ read.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+import read
+
+def load_data():
+    data = pd.read_csv('hn_stories.csv')
+    data.columns = ['submission_time', 'upvotes', 'url', 'headlines']
+    return data
+
+if __name__ == "__main__":
+    
+    load_data()
+    
+print(load_data().head())
