0c566ab830771961942219d920a699986fb7a4da
hameed <8092249+Youngsufi@users.noreply.github.com>
2019-01-17 23:38:50 -0500

    Update domains.py

--- domains.py
+++ domains.py
@@ -1,9 +1,13 @@
+import pandas as pd
+import numpy as np
 import read
+
 df = read.load_data()
 
 domains = df['url'].value_counts()
-for name,row in domains.items():
-    print("{0}: {1}".format(name,row))
+
+for name, row in domains.items():
+    print("{0}, {1}".format(name, row))
     
     if __name__ == "__main__":
         print
